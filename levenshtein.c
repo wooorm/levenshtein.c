845bab0926df6f1304dce8f835e3509c6ee28400
Michael Brockus <squidfarts@icloud.com>
2019-07-21 17:53:51 -0700

    Fixed data error.

--- levenshtein.c
+++ levenshtein.c
@@ -11,6 +11,19 @@
 // See <https://en.wikipedia.org/wiki/Levenshtein_distance> for more information.
 size_t
 levenshtein_n(const char *a, const size_t length, const char *b, const size_t bLength) {
+  // Shortcut optimizations / degenerate cases.
+  if (a == b) {
+      return 0;
+  }
+    
+  if (length == 0) {
+      return bLength;
+  }
+    
+  if (bLength == 0) {
+      return length;
+  }
+    
   size_t *cache = calloc(length, sizeof(size_t));
   size_t index = 0;
   size_t bIndex = 0;
@@ -18,19 +31,6 @@ levenshtein_n(const char *a, const size_t length, const char *b, const size_t bL
   size_t bDistance;
   size_t result;
   char code;
-
-  // Shortcut optimizations / degenerate cases.
-  if (a == b) {
-    return 0;
-  }
-
-  if (length == 0) {
-    return bLength;
-  }
-
-  if (bLength == 0) {
-    return length;
-  }
 
   // initialize the vector.
   while (index < length) {
